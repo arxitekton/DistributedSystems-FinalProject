7f80db8961d15e1fbc319b85a54e369738c6e509
Dmytro Maliarenko <dmytro.maliarenko@gmail.com>
2018-04-26 12:50:48 +0300

    modified Item

--- src/main/java/com/ucu/edu/store/Item.java
+++ src/main/java/com/ucu/edu/store/Item.java
@@ -3,22 +3,22 @@ package com.ucu.edu.store;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.PersistenceConstructor;
 
 @Data
-//@RequiredArgsConstructor(onConstructor_= {@PersistenceConstructor})
-//@RequiredArgsConstructor(onConstructor_={@AnnotationsGohere})
+@RequiredArgsConstructor(onConstructor = @__(@PersistenceConstructor))
 public class Item {
-
+	
+	@Id private String id;
+	
 	private final String caption;
 	private final double price;
 
 	int quantity = 1;
-
-	public Item(String caption, double price) {
-		this.caption = caption;
-		this.price = price;
-	}
 
 	public Item(String caption, double price, int quantity) {
 
